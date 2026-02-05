d1817ab5882fbf337a8e3694d3a043f5565aeda8
Domantas Dilys <sc17dd@leeds.ac.uk>
2026-02-05 14:58:16 +0000

    add sample datasets, readme file with execution instructions

---
+++ README.txt
@@ -0,0 +1,1 @@
+OMP_NUM_THREADS=1 ./ContourVisualiser -f ../Data/out_hh_24_coarsened_ascii.txt -o output -t 10 --decompositionType volume
--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@ mkdir -p Build-VTKm
 mkdir -p Installs
 cd Build-VTKm
 echo "Build and install the VTK-m codebase"
-cmake -DCMAKE_INSTALL_PREFIX=../Installs -DVTKm_USE_DOUBLE_PRECISION=ON  -DVTKm_ENABLE_OPENMP=ON -DCMAKE_CXX_FLAGS="-fopenmp" -DCMAKE_BUILD_TYPE=Release ../VTKm-main
+cmake -DCMAKE_INSTALL_PREFIX=../Installs -DVTKm_USE_DOUBLE_PRECISION=ON  -DVTKm_ENABLE_OPENMP=ON -DCMAKE_CXX_FLAGS="-fopenmp" -DCMAKE_BUILD_TYPE=Release ../VTK-m
 make -j20 install
 echo "Build and install the main contour-tree-augmented code used by flexible-isosurfaces (the contour-visualiser)"
-cd ../VTKm-main/examples/contour_tree_augmented/
+cd ../VTK-m/examples/contour_tree_augmented/
 mkdir -p Build
 cd Build
 cmake -DVTKm_USE_DOUBLE_PRECISION=ON  -DVTKm_ENABLE_OPENMP=ON -DCMAKE_CXX_FLAGS="-fopenmp" -DVTKm_DIR=../../../Installs/lib/cmake/vtkm-2.1/ -DCMAKE_INSTALL_PREFIX=../../../Installs ../
 make -j20 install
 echo "Build the flexible isosurfaces executable"
 cd ../../../../
-mkdir -p Flexible-Isosurfaces-Irregular
-cd Flexible-Isosurfaces-Irregular
-cmake -DVTKm_USE_DOUBLE_PRECISION=ON  -DVTKm_ENABLE_OPENMP=ON -DCMAKE_CXX_FLAGS="-fopenmp" -DVTKm_DIR=../Installs/lib/cmake/vtkm-2.1/ -DCMAKE_INSTALL_PREFIX=../Installs ../VTKm-main/examples/contour-visualiser
+mkdir -p Flexible-Isosurfaces-Regular
+cd Flexible-Isosurfaces-Regular
+cmake -DVTKm_USE_DOUBLE_PRECISION=ON  -DVTKm_ENABLE_OPENMP=ON -DCMAKE_CXX_FLAGS="-fopenmp" -DVTKm_DIR=../Installs/lib/cmake/vtkm-2.1/ -DCMAKE_INSTALL_PREFIX=../Installs ../VTK-m/examples/contour-visualiser
 make -j20
 
